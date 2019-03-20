7d07762274aef620c0f158923164c5b14d3af307
topspinppy <5806021623049@fitm.kmutnb.ac.th>
2019-03-20 15:43:10 +0700

    update mongo

--- docker-bootcamp/application/nodejs/index.js
+++ docker-bootcamp/application/nodejs/index.js
@@ -1,10 +1,26 @@
 var express = require("express");
-var app = express();
+const MongoClient = require("mongodb").MongoClient;
+const url = `mongodb://${process.env.DATABASE_USER}:${
+  process.env.DATABASE_PASSWORD
+}@${process.env.DATABASE_HOST}:27017`;
+console.log(url);
 
-app.get("/", (req, res) => {
-  res.send("Hello nodejs in docker version 2");
+var app = express();
+app.get("/", (req, resp) => {
+  MongoClient.connect(url, (err, client) => {
+    if (err) throw err;
+    console.log("Database connected!");
+
+    const db = client.db("shoppers");
+    db.collection("products")
+      .find()
+      .toArray((err, result) => {
+        if (err) throw err;
+        resp.send(result);
+      });
+  });
 });
 
 app.listen(3000, () => {
-  console.log("server listening on port 3000");
+  console.log("Server listening on port 3000");
 });
